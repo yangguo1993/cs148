0d94f53dca2129ac168000e0dda3c42eca77202b
Yang Guo <yguo2@silk2.uvm.edu>
2015-12-10 07:27:17 -0500

    third commit final project

--- assignment10/header.php
+++ assignment10/header.php
@@ -1,4 +1,4 @@
 <header>
-        <h1><a href="home.php"><img src="pokemonBanner.png"  alt="Pokemon Trainer"/></a></h1>
+    <h1><a href="index.php"><img src="pokemonBanner.png"  alt="Pokemon Trainer"/></a></h1>
 <!--http://sickr.files.wordpress.com/2013/08/pokemon_starters.png-->
 </header>
--- sitemap.php
+++ sitemap.php
@@ -4,6 +4,7 @@
 
     <h4>FINAL PROJECT</h4>
     <ul><li><a href="assignment10/index.php">index.php</a></li></ul>
+    <ul><li><a href="assignment10/SpecsDoc.pdf">SpecsDoc.php</a></li></ul>
     
     <h4>MISC</h4>
     <ul><li><a href="misc/friday.php">friday.php</a></li></ul>
